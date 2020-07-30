6e9984c4d646ad219c522820873910ddde62ce1b
Bartlomiej Gawrych <gawrych.bartlomiej@gmail.com>
2020-07-30 22:16:08 +0200

    Add macro to measure code block execution time

--- include/common.hpp
+++ include/common.hpp
@@ -18,4 +18,22 @@ typedef unsigned int uint;
 
 #define TIME(name) auto name = omp_get_wtime(); 
 
+#define LOOP_UNOPTIMIZER(var) __asm__ volatile("" : "+g" (var) : :);
+
+#define mpragma(...)  _Pragma(#__VA_ARGS__)
+
+#define BENCHMARK_STRUCTURE(warmup, rounds, elapsed, ...)                \
+        int unoptimizer = 0;                                             \
+        for(int warmup_i=0; warmup_i < warmup; warmup_i++){              \
+            {__VA_ARGS__}                                                \
+            LOOP_UNOPTIMIZER(unoptimizer)                                \
+        }                                                                \
+        auto start = omp_get_wtime();                                    \
+        for(int round_i=0; round_i < rounds; round_i++){                 \
+            {__VA_ARGS__}                                                \
+            LOOP_UNOPTIMIZER(unoptimizer)                                \
+        }                                                                \
+        auto end = omp_get_wtime();                                      \
+        auto elapsed = end - start;                                      \
+        Logger::INFO << elapsed << " XDDD ";      
 #endif
