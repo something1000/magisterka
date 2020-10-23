d76e0ee569636db9eef2803312e4c13199e90a4a
Bartlomiej G <bartlomiej.gawrych@intel.com>
2020-10-24 01:20:37 +0200

    Add parallel version of BatchNorm (reduction)

--- src/BatchNorm.cpp
+++ src/BatchNorm.cpp
@@ -10,17 +10,83 @@
 void BatchNorm::RunParallel() {
 
     auto excel = *this->file;
-    int warmup = 10;
+    int warmup = 5;
     int rounds = 1000;
 
+    BENCHMARK_STRUCTURE(
+        excel,      // name of csv logger
+        "Parallel",   // name of benchmark
+        warmup,     // name of warmup rounds variable
+        rounds,     // name of benchmark rounds variable
+        ELAPSED,    // variable name to store execution time
+        {
+            float* input_raw = input_data[0][0][0];
+            //# mini-batch mean
+            for(int i=0; i < C; ++i) {
+                mean[i] = 0.0f;
+                double sum_i = 0.0f;
+                mpragma(omp parallel for reduction(+:sum_i))
+                    for(int j=0; j < N; ++j) {
+                        for(int k=0; k < H; ++k) {
+                            for(int p=0; p < W; ++p) {
+                                sum_i += input_raw[i*H*W + j*C*H*W + k*W + p];
+                                //assert(&(input_data[j][i][k][p]) == &(input_raw[i*H*W + j*C*H*W + k*W + p]));
+                            }
+                        }
+                    }
+                mean[i] = sum_i / (1.0f*(N*H*W));
+            }
+
+            //# mini-batch variance
+            //variance = np.mean((arr - mean.reshape((1, 3, 1, 1))) ** 2, axis=(0, 2, 3))
+            for(int i=0; i < C; ++i) {
+                variance[i] = 0.0f;
+                double var_i = 0.0f;
+                mpragma(omp parallel for reduction(+:var_i))
+                for(int j=0; j < N; ++j) {
+                    for(int k=0; k < H; ++k) {
+                        for(int p=0; p < W; ++p) {
+                            auto in = input_raw[i*H*W + j*C*H*W + k*W + p];
+                            var_i += (in - mean[i]) * (in - mean[i]);
+                            //assert(&(input_data[j][i][k][p]) == &(input_raw[i*H*W + j*C*H*W + k*W + p]));
+                            //x_hat[j][i][k][p] = input[j][i][k][p] - mean[i];
+                        }
+                    }
+                }
+                variance[i] = var_i / (1.0f*(N*H*W));
+            }
+
+            float* norm_divider = new float[C];
+            for(int i=0; i < C; ++i){
+                norm_divider[i] = sqrt(variance[i] + EPS);
+            }
+
+            mpragma(omp parallel for collapse(4))
+            for(int i=0; i < C; ++i) {
+                for(int j=0; j < N; ++j) {
+                    for(int k=0; k < H; ++k) {
+                        for(int p=0; p < W; ++p) {
+                            output[j][i][k][p] = gamma[i] * ((input_data[j][i][k][p] - mean[i]) / norm_divider[i]) + beta[i];
+                        }
+                    }
+                }
+            }
+        }
+   )
+
+//    for(int i=0; i < N; ++i) {
+//        for(int j=0; j < C; ++j) {
+//            Print2DArray(output[i][j], H, W);
+//            std::cout << std::endl;
+//        }
+//    }
 }
 
 void BatchNorm::RunSerial() {
 
     auto excel = *this->file;
-    int warmup = 0;
-    int rounds = 1;
-
+    int warmup = 5;
+    int rounds = 1000;
 
     BENCHMARK_STRUCTURE(
         excel,      // name of csv logger
@@ -29,12 +95,15 @@ void BatchNorm::RunSerial() {
         rounds,     // name of benchmark rounds variable
         ELAPSED,    // variable name to store execution time
         {
+            float* input_raw = input_data[0][0][0];
             //# mini-batch mean
             for(int i=0; i < C; ++i) {
+                mean[i] = 0.0f;
                 for(int j=0; j < N; ++j) {
                     for(int k=0; k < H; ++k) {
                         for(int p=0; p < W; ++p) {
-                            mean[i] += input_data[j][i][k][p];
+                            mean[i] += input_raw[i*H*W + j*C*H*W + k*W + p];
+                            assert(&(input_data[j][i][k][p]) == &(input_raw[i*H*W + j*C*H*W + k*W + p]));
                         }
                     }
                 }
@@ -44,10 +113,13 @@ void BatchNorm::RunSerial() {
             //# mini-batch variance
             //variance = np.mean((arr - mean.reshape((1, 3, 1, 1))) ** 2, axis=(0, 2, 3))
             for(int i=0; i < C; ++i) {
+                variance[i] = 0.0f;
                 for(int j=0; j < N; ++j) {
                     for(int k=0; k < H; ++k) {
                         for(int p=0; p < W; ++p) {
-                            variance[i] += (input_data[j][i][k][p] - mean[i]) * (input_data[j][i][k][p] - mean[i]);
+                            auto in = input_raw[i*H*W + j*C*H*W + k*W + p];
+                            variance[i] += (in - mean[i]) * (in - mean[i]);
+                            assert(&(input_data[j][i][k][p]) == &(input_raw[i*H*W + j*C*H*W + k*W + p]));
                             //x_hat[j][i][k][p] = input[j][i][k][p] - mean[i];
                         }
                     }
@@ -68,19 +140,25 @@ void BatchNorm::RunSerial() {
                         }
                     }
                 }
-                variance[i] = variance[i]/(N*H*W);
             }
         }
    )
+//     for(int i=0; i < N; ++i) {
+//        for(int j=0; j < C; ++j) {
+//            Print2DArray(output[i][j], H, W);
+//            std::cout << std::endl;
+//        }
+//    }
 }
 
 void BatchNorm::Init(Logger::LoggerClass* file) {
     this->file = file;
-    N = 2;
+    N = 32;
     C = 3;
-    H = 4;
-    W = 4;
+    H = 224;
+    W = 224;
     input_data = Create4DArray<float>(N, C, H, W);
+    FillRandom4DArray(input_data, N, C, H, W);
     gamma = new float[C];
     beta = new float[C];
     output = Create4DArray<float>(N, C, H, W);
