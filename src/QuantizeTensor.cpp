5ac361f0de4809c0201d8508ce071eee22f2956a
Bartlomiej Gawrych <gawrych.bartlomiej@gmail.com>
2020-12-02 16:13:13 +0100

    Fix Quantize Tensor benchmark + Add 2 openmp cases

--- src/QuantizeTensor.cpp
+++ src/QuantizeTensor.cpp
@@ -5,6 +5,14 @@
 #include <functional>
 
 void QuantizeTensor::RunParallel() {
+    RunParallel_1();
+    RunParallel_2();
+    RunParallel_3();
+}
+
+
+
+void QuantizeTensor::RunParallel_1() {
 
     auto excel = *this->file;
     int warmup = 5;
@@ -14,14 +22,69 @@ void QuantizeTensor::RunParallel() {
     int8_t* raw_output = output[0][0][0];
     BENCHMARK_STRUCTURE(
         excel,      // name of csv logger
-        "Parallel",   // name of benchmark
+        "PARALLEL_FOR_COLLAPSE",   // name of benchmark
         warmup,     // name of warmup rounds variable
         rounds,     // name of benchmark rounds variable
         ELAPSED,    // variable name to store execution time
         {
-            mpragma(omp simd)
-            for(int i=0; i < N*C*H*W; i++) {
-                raw_output[i] = (raw_input[i]/scale) + zero_position;
+            mpragma(omp parallel for collapse(4) schedule(static, 100))
+            for(int j=0; j < N; ++j) {
+                for(int z=0; z < C; ++z) {
+                    for(int k=0; k < H; ++k) {
+                        for(int p=0; p < W; ++p) {
+                            output[j][z][k][p] = static_cast<int8_t>((input[j][z][k][p]/scale) + zero_position);
+                        }
+                    }
+                }
+            }
+        }
+   )
+}
+
+void QuantizeTensor::RunParallel_2() {
+
+    auto excel = *this->file;
+    int warmup = 5;
+    int rounds = 1000;
+
+    float* raw_input = input[0][0][0];
+    int8_t* raw_output = output[0][0][0];
+    BENCHMARK_STRUCTURE(
+        excel,      // name of csv logger
+        "PARALLEL_FOR",   // name of benchmark
+        warmup,     // name of warmup rounds variable
+        rounds,     // name of benchmark rounds variable
+        ELAPSED,    // variable name to store execution time
+        {
+            int size = N*C*H*W;
+            mpragma(omp parallel for schedule(static, 100))
+            for(int i=0; i < size; i++) {
+                raw_output[i] = static_cast<int8_t>((raw_input[i]/scale) + zero_position);
+            }
+        }
+   )
+}
+
+
+void QuantizeTensor::RunParallel_3() {
+
+    auto excel = *this->file;
+    int warmup = 5;
+    int rounds = 1000;
+
+    float* raw_input = input[0][0][0];
+    int8_t* raw_output = output[0][0][0];
+    BENCHMARK_STRUCTURE(
+        excel,      // name of csv logger
+        "PARALLEL_FOR_SIMD",   // name of benchmark
+        warmup,     // name of warmup rounds variable
+        rounds,     // name of benchmark rounds variable
+        ELAPSED,    // variable name to store execution time
+        {
+            int size = N*C*H*W;
+            mpragma(omp parallel for simd)
+            for(int i=0; i < size; i++) {
+                raw_output[i] = static_cast<int8_t>((raw_input[i]/scale) + zero_position);
             }
         }
    )
