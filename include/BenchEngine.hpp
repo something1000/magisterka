0fe7f87095e638ea824ccc08b5fa09b6353807bb
Bartlomiej G <bartlomiej.gawrych@intel.com>
2020-12-12 00:12:45 +0100

    Add json based benchmark descriptors

--- include/BenchEngine.hpp
+++ include/BenchEngine.hpp
@@ -1,10 +1,21 @@
 #ifndef _BENCHENGINE_HPP
 #define _BENCHENGINE_HPP
 #include "common.hpp"
-
+#include "Benchmark.hpp"
+#include "EmptyForLoopBenchmark.hpp"
+#include "MatrixMultiplication.hpp"
+#include "Convolution2D.hpp"
+#include "QuickSort.hpp"
+#include "BatchNorm.hpp"
+#include "QuantizeTensor.hpp"
+#include "CooleyTukeyFFT.hpp"
+#include "WaveEquation.hpp"
+ 
 class BenchEngine {
     public:
-        static void Start(BenchmarkMap benchmarks);
+        static void Start(JsonPtr json);
+        static BenchmarkPtr GetBenchmark(std::string name); 
+    private:
 
 };
 #endif
--- src/BenchEngine.cpp
+++ src/BenchEngine.cpp
@@ -1,22 +1,29 @@
 #include <iostream>
 #include "BenchEngine.hpp"
-#include "common.hpp"
 
-void BenchEngine::Start(BenchmarkMap benchmarks) {
+#include <stdexcept>
+
+using JsonBenchmark = rapidjson::Value::ConstValueIterator;
+
+void BenchEngine::Start(JsonPtr json) {
     std::fstream excelStream("excelraport.csv", std::ios::out);
     auto excel = Logger::EXCEL(excelStream);
 
     Logger::INFO << "Starting benchmark engine!";
     Logger::INFO << "Executing following benchmarks:";
-    for(auto& [name, bench] : benchmarks) {
-        Logger::INFO << " \t- " << name;
+    
+    for (JsonBenchmark itr = json->Begin(); itr != json->End(); ++itr) {
+         Logger::INFO << " \t- " << itr->GetObject()["name"].GetString();
     }
 
     // Create Excel Columns Titles
     excel << "Name" << "Warmup" << "Iterations" << "Duration";
     excel.newLine();
 
-    for(auto& [name, bench] : benchmarks) {
+    for (JsonBenchmark itr = json->Begin(); itr != json->End(); ++itr) {
+        auto benchDescriptor = itr->GetObject();
+        auto name = benchDescriptor["name"].GetString();
+        auto bench = GetBenchmark(name);
         Logger::INFO << "[" << name << "] " << "Initializing benchmark";
         bench->Init(&excel);
         Logger::INFO << "[" << name << "] " << "Starting benchmark";
@@ -30,5 +37,30 @@ void BenchEngine::Start(BenchmarkMap benchmarks) {
         Logger::INFO << "[" << name << "] " << "Finished serial benchmark";
         excel.newLine();
         Logger::INFO << "==============================================================";
-    }
+     }
+}
+
+
+std::shared_ptr<Benchmark> BenchEngine::GetBenchmark(std::string name) {
+    std::shared_ptr<Benchmark> ptr;
+    if (name == "EmptyForLoopBenchmark")
+        ptr = std::make_shared<EmptyForLoopBenchmark>(name);
+    else if (name == "MatrixMultiplication")
+        ptr = std::make_shared<MatrixMultiplication>(name);
+    else if (name == "Convolution2D")
+        ptr = std::make_shared<Convolution2D>(name);
+    else if (name == "QuickSort")
+        ptr = std::make_shared<QuickSort>(name);
+    else if (name == "BatchNorm")
+        ptr = std::make_shared<BatchNorm>(name);
+    else if (name == "QuantizeTensor")
+        ptr = std::make_shared<QuantizeTensor>(name);
+    else if (name == "CooleyTukeyFFT")
+        ptr = std::make_shared<CooleyTukeyFFT>(name);
+    else if (name == "WaveEquation")
+        ptr = std::make_shared<WaveEquation>(name);
+    else
+        throw std::invalid_argument("No such benchmark as " + name);
+    
+    return ptr;
 }
