f4fe86c51cb6a0b9ba4baa7345c380e8abdeeab6
Bartlomiej G <bartlomiej.gawrych@intel.com>
2020-12-18 23:03:27 +0100

    Add BatchNorm benchmark with simd operations

--- src/BatchNorm.cpp
+++ src/BatchNorm.cpp
@@ -8,12 +8,88 @@
 #define EPS 0.00000001f
 
 void BatchNorm::RunParallel() {
+    RunParallel_1();
+    RunParallel_2();
+}
 
+void BatchNorm::RunParallel_2() {
     auto excel = *this->file;
 
     BENCHMARK_STRUCTURE(
         excel,      // name of csv logger
-        "Parallel",   // name of benchmark
+        "Parallel_2",   // name of benchmark
+        warmup,     // name of warmup rounds variable
+        rounds,     // name of benchmark rounds variable
+        ELAPSED,    // variable name to store execution time
+        {
+            float* input_raw = input_data[0][0][0];
+            //# mini-batch mean
+            for(int i=0; i < C; ++i) {
+                mean[i] = 0.0f;
+                double sum_i = 0.0f;
+                mpragma(omp parallel for simd reduction(+:sum_i) collapse(3))
+                    for(int j=0; j < N; ++j) {
+                        for(int k=0; k < H; ++k) {
+                            for(int p=0; p < W; ++p) {
+                                sum_i += input_raw[i*H*W + j*C*H*W + k*W + p];
+                                //assert(&(input_data[j][i][k][p]) == &(input_raw[i*H*W + j*C*H*W + k*W + p]));
+                            }
+                        }
+                    }
+                mean[i] = sum_i / (1.0f*(N*H*W));
+            }
+
+            //# mini-batch variance
+            //variance = np.mean((arr - mean.reshape((1, 3, 1, 1))) ** 2, axis=(0, 2, 3))
+            for(int i=0; i < C; ++i) {
+                variance[i] = 0.0f;
+                double var_i = 0.0f;
+                mpragma(omp parallel for reduction(+:var_i) collapse(3))
+                for(int j=0; j < N; ++j) {
+                    for(int k=0; k < H; ++k) {
+                        for(int p=0; p < W; ++p) {
+                            auto in = input_raw[i*H*W + j*C*H*W + k*W + p];
+                            var_i += (in - mean[i]) * (in - mean[i]);
+                            //assert(&(input_data[j][i][k][p]) == &(input_raw[i*H*W + j*C*H*W + k*W + p]));
+                            //x_hat[j][i][k][p] = input[j][i][k][p] - mean[i];
+                        }
+                    }
+                }
+                variance[i] = var_i / (1.0f*(N*H*W));
+            }
+
+            float* norm_divider = new float[C];
+            for(int i=0; i < C; ++i){
+                norm_divider[i] = sqrt(variance[i] + EPS);
+            }
+
+            mpragma(omp parallel for collapse(4))
+            for(int i=0; i < C; ++i) {
+                for(int j=0; j < N; ++j) {
+                    for(int k=0; k < H; ++k) {
+                        for(int p=0; p < W; ++p) {
+                            output[j][i][k][p] = gamma[i] * ((input_data[j][i][k][p] - mean[i]) / norm_divider[i]) + beta[i];
+                        }
+                    }
+                }
+            }
+        }
+   )
+
+//    for(int i=0; i < N; ++i) {
+//        for(int j=0; j < C; ++j) {
+//            Print2DArray(output[i][j], H, W);
+//            std::cout << std::endl;
+//        }
+//    }
+}
+
+void BatchNorm::RunParallel_1() {
+    auto excel = *this->file;
+
+    BENCHMARK_STRUCTURE(
+        excel,      // name of csv logger
+        "Parallel_1",   // name of benchmark
         warmup,     // name of warmup rounds variable
         rounds,     // name of benchmark rounds variable
         ELAPSED,    // variable name to store execution time
