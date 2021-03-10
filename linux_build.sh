2ab0462f0f2da4968f44e6fbbdd6efce71cc3be5
B.Gawrych <gawrych.bartlomiej@gmail.com>
2021-03-10 09:38:07 +0100

    Select compiler by linux_build.sh argument

--- linux_build.sh
+++ linux_build.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+rm -rf build
+
+if [[ "$1" = "icc" ]] ; then
+    export CC=icc
+    export CXX=icpc
+elif [[ "$1" = "clang" ]] ; then
+    export CC=clang
+    export CXX=clang++
+else 
+    export CC=gcc
+    export CXX=g++
+fi
+
 mkdir -p build
 cd build
 cmake -GNinja ..
