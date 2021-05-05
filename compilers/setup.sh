5a1c9fd1fb04855779fae4ea6ae638822cf7f761
B.Gawrych <gawrych.bartlomiej@gmail.com>
2021-05-05 10:26:24 +0200

    Fix 'GLIBCXX_3.4.26 not found' error

--- compilers/setup.sh
+++ compilers/setup.sh
@@ -8,7 +8,7 @@ UTILS_PATH=$WORKDIR/utils
 
 
 export PATH=$APPS_PATH/bin:$PATH
-export LD_LIBRARY_PATH=$APPS_PATH/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$APPS_PATH/lib64:$APPS_PATH/lib:$LD_LIBRARY_PATH
 
 
 #clang vector.c -fopenmp -fopenmp-targets=nvptx64-nvidia-cuda
