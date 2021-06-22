5e85f72f8ae0bf9fcf08822e56cee2d168629d37
Bartlomiej Gawrych <gawrych.bartlomiej@gmail.com>
2021-06-22 19:47:20 +0200

    Update offload descriptors

--- openmp_benchmarks/target/src/QuickSort.cpp
+++ openmp_benchmarks/target/src/QuickSort.cpp
@@ -5,8 +5,8 @@
 #include <functional>
 
 void QuickSort::RunParallel() {
-    RunParallelDouble();
-    RunParallelSingle();
+//    RunParallelDouble();
+//    RunParallelSingle();
 }
 
 float PartitionArray(float *array, int left, int right) {
@@ -37,24 +37,24 @@ void ParallelQuicksortDouble(float* array, int left, int right, int node_id) {
 }
 
 void QuickSort::RunParallelDouble() {
-    auto excel = *this->file;
-
-    BENCHMARK_STRUCTURE(
-        excel,      // name of csv logger
-        "Parallel_Double",   // name of benchmark
-        warmup,     // name of warmup rounds variable
-        rounds,     // name of benchmark rounds variable
-        ELAPSED,    // variable name to store execution time
-        {
-            std::memcpy(data, input_data, size * sizeof(float));
-            mpragma(omp parallel shared(data, size)) {
-                mpragma(omp single) {
-                    ParallelQuicksortDouble(data, 0, size-1, 1);
-                }
-                mpragma(omp taskwait)
-            }
-        }
-   )
+//    auto excel = *this->file;
+//
+//    BENCHMARK_STRUCTURE(
+//        excel,      // name of csv logger
+//        "Parallel_Double",   // name of benchmark
+//        warmup,     // name of warmup rounds variable
+//        rounds,     // name of benchmark rounds variable
+//        ELAPSED,    // variable name to store execution time
+//        {
+//            std::memcpy(data, input_data, size * sizeof(float));
+//            mpragma(omp parallel shared(data, size)) {
+//                mpragma(omp single) {
+//                    ParallelQuicksortDouble(data, 0, size-1, 1);
+//                }
+//                mpragma(omp taskwait)
+//            }
+//        }
+//   )
 //    Print2DArray(&data, 1, size);
 //    std::cout << "\n\n=============\n\n";
 }
@@ -72,24 +72,24 @@ void ParallelQuicksortSingle(float* array, int left, int right, int node_id) {
     }
 }
 void QuickSort::RunParallelSingle() {
-    auto excel = *this->file;
-
-    BENCHMARK_STRUCTURE(
-        excel,      // name of csv logger
-        "Parallel_Single",   // name of benchmark
-        warmup,     // name of warmup rounds variable
-        rounds,     // name of benchmark rounds variable
-        ELAPSED,    // variable name to store execution time
-        {
-            std::memcpy(data, input_data, size * sizeof(float));
-            mpragma(omp parallel shared(data, size)) {
-                mpragma(omp single) {
-                    ParallelQuicksortSingle(data, 0, size-1, 1);
-                }
-                mpragma(omp taskwait)
-            }
-        }
-   )
+//    auto excel = *this->file;
+//
+//    BENCHMARK_STRUCTURE(
+//        excel,      // name of csv logger
+//        "Parallel_Single",   // name of benchmark
+//        warmup,     // name of warmup rounds variable
+//        rounds,     // name of benchmark rounds variable
+//        ELAPSED,    // variable name to store execution time
+//        {
+//            std::memcpy(data, input_data, size * sizeof(float));
+//            mpragma(omp parallel shared(data, size)) {
+//                mpragma(omp single) {
+//                    ParallelQuicksortSingle(data, 0, size-1, 1);
+//                }
+//                mpragma(omp taskwait)
+//            }
+//        }
+//   )
 //    Print2DArray(&data, 1, size);
 //    std::cout << "\n\n=============\n\n";
 }
@@ -103,79 +103,79 @@ void Quicksort(float *array, int left, int right){
 }
 
 void QuickSort::RunSerial() {
-    auto excel = *this->file;
-
-
-    BENCHMARK_STRUCTURE(
-        excel,      // name of csv logger
-        "Serial",   // name of benchmark
-        warmup,     // name of warmup rounds variable
-        rounds,     // name of benchmark rounds variable
-        ELAPSED,    // variable name to store execution time
-        {
-            std::memcpy(data, input_data, size * sizeof(float));
-            Quicksort(data, 0, size-1);
-        }
-   )
+//    auto excel = *this->file;
+//
+//
+//    BENCHMARK_STRUCTURE(
+//        excel,      // name of csv logger
+//        "Serial",   // name of benchmark
+//        warmup,     // name of warmup rounds variable
+//        rounds,     // name of benchmark rounds variable
+//        ELAPSED,    // variable name to store execution time
+//        {
+//            std::memcpy(data, input_data, size * sizeof(float));
+//            Quicksort(data, 0, size-1);
+//        }
+//   )
     // Print2DArray(&data, 1, size);
     // std::cout << "\n\n=============\n\n";
 }
 
 bool QuickSort::Validate() {
-    float* out_serial = new float[size];
-    float* out_parallel_1 = new float[size];
-    float* out_parallel_2 = new float[size];
-
-    rounds = 1;
-    warmup = 0;
-
-    float* tmp = data;
-
-    data = out_serial;
-    RunSerial();
-
-    data = out_parallel_1;
-    RunParallelSingle();
-
-    data = out_parallel_2;
-    RunParallelDouble();
-
-    bool is_valid = CompareArray(out_serial, out_parallel_1, size);
-    is_valid &= CompareArray(out_serial, out_parallel_2, size);
-
-    data = tmp;
-    delete[] out_serial;
-    delete[] out_parallel_1;
-    delete[] out_parallel_2;
-
-    return is_valid;
+//    float* out_serial = new float[size];
+//    float* out_parallel_1 = new float[size];
+//    float* out_parallel_2 = new float[size];
+//
+//    rounds = 1;
+//    warmup = 0;
+//
+//    float* tmp = data;
+//
+//    data = out_serial;
+//    RunSerial();
+//
+//    data = out_parallel_1;
+//    RunParallelSingle();
+//
+//    data = out_parallel_2;
+//    RunParallelDouble();
+//
+//    bool is_valid = CompareArray(out_serial, out_parallel_1, size);
+//    is_valid &= CompareArray(out_serial, out_parallel_2, size);
+//
+//    data = tmp;
+//    delete[] out_serial;
+//    delete[] out_parallel_1;
+//    delete[] out_parallel_2;
+//
+//    return is_valid;
 }
 
 void QuickSort::Init(Logger::LoggerClass* file, const rapidjson::Value& properties) {
-    this->file = file;
-    rounds = properties["rounds"].GetInt();
-    warmup = properties["warmup"].GetInt();
-    size = properties["size"].GetInt();
-    Logger::INFO << VAR(size);
-
-    Reinitialize();
+//    this->file = file;
+//    rounds = properties["rounds"].GetInt();
+//    warmup = properties["warmup"].GetInt();
+//    size = properties["size"].GetInt();
+//    Logger::INFO << VAR(size);
+//
+//    Reinitialize();
 }
 
 
 void QuickSort::Reinitialize() {
-    if(initialized) {
-        delete[] input_data;
-        delete[] data;
-    }
-
-    input_data = new float[size];
-    data = new float[size];
-    FillRandomArray(input_data, size);
-    this->initialized = true;
+//    if(initialized) {
+//        delete[] input_data;
+//        delete[] data;
+//    }
+//
+//    input_data = new float[size];
+//    data = new float[size];
+//    FillRandomArray(input_data, size);
+//    this->initialized = true;
 }
 
-static std::shared_ptr<Benchmark> CreateBench() {
-    return std::make_shared<QuickSort>("QuickSort");
-}
+//static std::shared_ptr<Benchmark> CreateBench() {
+//    return std::make_shared<QuickSort>("QuickSort");
+//}
 
-REGISTER_BENCHMARK(QuickSort, CreateBench);
+//REGISTER_BENCHMARK(QuickSort, CreateBench);
